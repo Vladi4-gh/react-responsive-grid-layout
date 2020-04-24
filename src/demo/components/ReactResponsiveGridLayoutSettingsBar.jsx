558357c11c87b06d5407b676118bffd3ff1d5bb9
Vladislav Ivanov <vladi4.gm@gmail.com>
2020-04-24 03:50:41 +0300

    Switchable mock data settings panel has been implemented.

--- src/demo/components/ReactResponsiveGridLayoutSettingsBar.jsx
+++ src/demo/components/ReactResponsiveGridLayoutSettingsBar.jsx
@@ -3,12 +3,45 @@ import { connect } from "react-redux";
 import { setCurrentMockDataSetId } from "../actions/mockData";
 
 class ReactResponsiveGridLayoutSettingsBar extends React.Component {
-    constructor() {
-        super();
+    state = {
+        isSettingsBarOpened: false
+    }
 
-        this.state = {
-            isOpened: false
+    renderOpenSettingsBarButton = () => {
+        const onClick = () => {
+            this.setState(() => ({
+                isSettingsBarOpened: true
+            }));
         };
+
+        return this.state.isSettingsBarOpened ? null : (
+            <button onClick={onClick}>
+                Current mock data is "{this.props.currentMockDataSet.name}". Click here to change.
+            </button>
+        );
+    }
+
+    renderCloseSettingsBarButton = () => {
+        const onClick = () => {
+            this.setState(() => ({
+                isSettingsBarOpened: false
+            }));
+        };
+
+        return (
+            <button onClick={onClick}>
+                Close
+            </button>
+        );
+    }
+
+    renderSettingsBar = () => {
+        return !this.state.isSettingsBarOpened ? null : (
+            <div className="react-responsive-grid-layout-settings-bar">
+                {this.renderMockDataSetsDropdownList()}
+                {this.renderCloseSettingsBarButton()}
+            </div>
+        );
     }
 
     renderMockDataSetsDropdownList = () => {
@@ -18,7 +51,7 @@ class ReactResponsiveGridLayoutSettingsBar extends React.Component {
 
         return (
             <select
-                value={this.props.currentMockDataSetId}
+                value={this.props.currentMockDataSet.id}
                 onChange={onChange}
             >
                 {this.props.mockDataSetsInfo.map((mockDataSetInfo) => (
@@ -35,25 +68,30 @@ class ReactResponsiveGridLayoutSettingsBar extends React.Component {
 
     render() {
         return (
-            <div className="react-responsive-grid-layout-settings-bar">
-                {this.renderMockDataSetsDropdownList()}
+            <div className="react-responsive-grid-layout-settings-bar-container">
+                {this.renderOpenSettingsBarButton()}
+                {this.renderSettingsBar()}
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => ({
-    currentMockDataSetId: state
-        .mockData
-        .currentMockDataSetId,
-    mockDataSetsInfo: state
+const mapStateToProps = (state) => {
+    const mockDataSetsInfo = state
         .mockData
         .mockDataSets
         .map((mockDataSet) => ({
             id: mockDataSet.id,
             name: mockDataSet.name
-        }))
-});
+        }));
+    const currentMockDataSet = mockDataSetsInfo
+        .find((mockDataSetInfo) => mockDataSetInfo.id === state.mockData.currentMockDataSetId);
+
+    return {
+        currentMockDataSet,
+        mockDataSetsInfo
+    };
+};
 
 const mapDispatchToProps = (dispatch) => ({
     setCurrentMockDataSetId: (id) => dispatch(setCurrentMockDataSetId(id))
